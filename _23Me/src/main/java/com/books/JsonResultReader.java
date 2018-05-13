071712650339204d5a0725715f202aa8a508f73d
ashBose <bose.aishwarya.0210@gmail.com>
2018-05-12 23:05:10 -0700

    Update JsonResultReader.java

--- _23Me/src/main/java/com/books/JsonResultReader.java
+++ _23Me/src/main/java/com/books/JsonResultReader.java
@@ -1,4 +1,29 @@
 package com.books;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.stream.JsonReader;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+
 public class JsonResultReader {
+    private List<String> bookShelf;
+    private JsonReader reader = null;
+
+    JsonResultReader(String fileName) throws IOException {
+        try{
+            reader = new JsonReader(new FileReader(fileName));
+        } catch (IOException e) {
+            throw new IOException("unable to open file");
+        }
+    }
+
+    public List<String> getVirtualShelf() throws IOException{
+        Gson gson = new GsonBuilder().create();
+        bookShelf = gson.fromJson(reader, List.class);
+        return bookShelf;
+    }
+
 }
